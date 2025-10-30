4a5dc4616ec7198b0ea9cad46fba7b3b0c436ac5
tack0sim <talhamjd@gmail.com>
2025-10-30 16:15:20 +0100

    add: basic idempotency to content migration

--- apps/studio/migrations/replace-event-type-with-event-format/index.ts
+++ apps/studio/migrations/replace-event-type-with-event-format/index.ts
@@ -1,15 +1,29 @@
-import {defineMigration, at, setIfMissing, unset} from 'sanity/migrate'
+import {defineMigration, at, setIfMissing, unset, insert} from 'sanity/migrate'
 
 const from = 'eventType'
 const to = 'format'
 
+// should be uinque for the migration but never change
+const idempotenceKey = 'xyz'
+
 export default defineMigration({
   title: 'Replace event type with event format',
-  documentTypes: ['event'],
+  // documentTypes: ['event'],
+  filter: '_type == "event" && defined(eventType) && !defined(format)',
 
   migrate: {
     document(doc, context) {
-      return [at(to, setIfMissing(doc[from])), at(from, unset())]
+      if (((doc?._migrations as string[]) || []).includes(idempotenceKey)) {
+        // Document already migrated, so we can skip
+        return
+      }
+      return [
+        at(to, setIfMissing(doc[from])),
+        at(from, unset()),
+        // add idempotence key
+        at('_migrations', setIfMissing([])),
+        at('_migrations', insert(idempotenceKey, 'after', 0)),
+      ]
     },
   },
 })
