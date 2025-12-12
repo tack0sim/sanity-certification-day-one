90e7144819a49223de17ef36174eaed095f8f9d5
tack0sim <talhamjd@gmail.com>
2025-12-12 12:01:30 +0100

    fix: sanity config

--- apps/studio/sanity.config.ts
+++ apps/studio/sanity.config.ts
@@ -12,8 +12,8 @@ export default defineConfig({
   name: 'default',
   title: 'Day One Content Operations',
 
-  projectId: process.env.SANITY_STUDIO_PROJECT_ID,
-  dataset: process.env.SANITY_STUDIO_DATASET,
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID as string,
+  dataset: process.env.SANITY_STUDIO_DATASET as string,
   schema,
 
   plugins: [
